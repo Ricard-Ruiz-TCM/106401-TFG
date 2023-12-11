e7de27a02146fe689faac0014eee6d7eb5b0807c
Ricard <rruizc@edu.tecnocampus.cat>
2023-12-11 01:59:19 +0100

    addicion de movidas para los spakers y others

--- Assets/Scripts/Controllers/Background.cs
+++ Assets/Scripts/Controllers/Background.cs
@@ -5,21 +5,16 @@ public class Background : MonoBehaviour {
 
     [SerializeField]
     private string _fileName;
-    private string _tmp;
 
-    // Image background
+    [SerializeField]
     private Image _background;
-
-    // Unity Awake
-    private void Awake() {
-        _background = GetComponent<Image>();
-    }
 
     // Unity Update
     private void Update() {
-        GameManager.instance.GetValue<string>("$background", out _tmp);
-        if ((_tmp != "") && (_tmp != _fileName)) {
-            _fileName = _tmp;
+        string str = _fileName;
+        GameManager.instance.GetValue<string>("$background", out str);
+        if ((str != "") && (str != _fileName)) {
+            _fileName = str;
             _background.sprite = Resources.Load<Sprite>("background/" + _fileName);
         }
     }
---
+++ Assets/Scripts/Controllers/Characters.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Characters : MonoBehaviour {
+
+    // Speaker
+    [SerializeField]
+    private string _otherFileName;
+    [SerializeField]
+    private string _speakerFileName;
+
+    [SerializeField, Header("Speaker:")]
+    private Image _speaker;
+
+    [SerializeField, Header("Others:")]
+    private GameObject _others;
+    [SerializeField]
+    private GameObject _speakerPrefab;
+
+    // Unity Update
+    private void Update() {
+
+        // Speaker
+        string str = _speakerFileName;
+        GameManager.instance.GetValue<string>("$speaker", out str);
+        if ((str != "") && (str != _speakerFileName)) {
+            _speakerFileName = str;
+            _speaker.sprite = Resources.Load<Sprite>("characters/" + _speakerFileName);
+            foreach (Transform t in _others.transform) {
+                if (t.name == _speakerFileName) {
+                    GameObject.Destroy(t.gameObject);
+                }
+            }
+        }
+
+        // Others
+        str = _otherFileName;
+        GameManager.instance.GetValue<string>("$speaker_others", out str);
+        if ((str != "") && (str != _otherFileName)) {
+            foreach (Transform t in _others.transform) {
+                GameObject.Destroy(t.gameObject);
+            }
+            _otherFileName = str;
+            Debug.Log(_otherFileName);
+            foreach (string s in _otherFileName.Split('|')) {
+                if (s != _speakerFileName) {
+                    Debug.Log(s);
+                    GameObject g = GameObject.Instantiate(_speakerPrefab, _others.transform);
+                    g.GetComponent<Image>().sprite = Resources.Load<Sprite>("characters/" + s);
+                    g.name = s;
+                }
+            }
+        }
+
+    }
+}
