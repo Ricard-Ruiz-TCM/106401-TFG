9cc6fa0c9484839d5780324312bca548f8c5aaca
Ricard <rruizc@edu.tecnocampus.cat>
2023-12-11 12:26:17 +0100

    cositas

--- Assets/Scripts/Controllers/Characters.cs
+++ Assets/Scripts/Controllers/Characters.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class Characters : MonoBehaviour {
 
+    [SerializeField]
     private string _otherFileName;
 
     [SerializeField, Header("Speaker:")]
@@ -21,31 +22,28 @@ public class Characters : MonoBehaviour {
     private void Update() {
 
         // Speaker
-        string name = (_speakerFileName != null ? _speakerFileName.text.ToLower() : "");
-        if (name.IndexOf('(') != -1) {
-            name = name.Substring(0, name.IndexOf(' '));
-        }
-        if (name != "") {
-            _speaker.sprite = Resources.Load<Sprite>("characters/" + name);
-        }
-
-        // others activation
-        foreach (Transform t in _others.transform) {
-            t.gameObject.SetActive(false);
-            if (t.name != name) {
-                t.gameObject.SetActive(true);
-            }
-        }
+        string name = (_speakerFileName.IsActive() ? _speakerFileName.text.ToLower() : "");
+        if (name.IndexOf('(') != -1) { name = name.Substring(0, name.IndexOf(' ')); }
+        _speaker.sprite = Resources.Load<Sprite>("characters/" + name);
+        _speaker.gameObject.SetActive(_speakerFileName.IsActive());
+        if (!_speaker.IsActive()) name = "";
 
         // Others
         string str = _otherFileName;
         GameManager.instance.GetValue<string>("$speaker_others", out str);
+        foreach (Transform t in _others.transform) {
+            if ((t.name == name) || (name == "")) {
+                t.gameObject.SetActive(false);
+            } else {
+                t.gameObject.SetActive(true);
+            }
+        }
+
         if ((str != "") && (str != _otherFileName)) {
             foreach (Transform t in _others.transform) {
                 GameObject.Destroy(t.gameObject);
             }
             _otherFileName = str;
-            Debug.Log(_otherFileName);
             foreach (string s in _otherFileName.Split('|')) {
                 GameObject g = GameObject.Instantiate(_speakerPrefab, _others.transform);
                 g.GetComponent<Image>().sprite = Resources.Load<Sprite>("characters/" + s);
